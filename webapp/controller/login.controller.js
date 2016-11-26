7c04bc402777d2bc6ef2f20529c1e392cdaa80dd
Prateek057 <bagrecha.prateek@gmail.com>
2016-11-26 14:07:09 +0000

    Login view created

--- webapp/controller/login.controller.js
+++ webapp/controller/login.controller.js
@@ -3,7 +3,7 @@ sap.ui.define([
 ], function(Controller) {
 	"use strict";
 
-	return Controller.extend("BikeRentalApp.controller.View1", {
+	return Controller.extend("BikeRentalApp.controller.login", {
 
 	});
 });
