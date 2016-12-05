eaac90d720feb08f679b0def627ae9c1f27ab56c
Prateek057 <bagrecha.prateek@gmail.com>
2016-12-05 13:43:16 +0000

    Location Marker Code added

--- webapp/controller/searchbike.controller.js
+++ webapp/controller/searchbike.controller.js
@@ -28,20 +28,8 @@ sap.ui.define([
 		 * This hook is the same one that SAPUI5 controls get after being rendered.
 		 * @memberOf BikeRentalApp.view.searchbike
 		 */
-			onAfterRendering: function() {
-			/*jQuery.sap.require('openui5.googlemaps.MapUtils');
-			jQuery.sap.require("sap.ui.core.IconPool");
-			var aNames = sap.ui.core.IconPool.getIconNames();
-			var icon = new sap.ui.core.Icon({
-				src: sap.ui.core.IconPool.getIconURI(aNames[0]),
-				size: "32px",
-				color: "#333333",
-				activeColor: "white",
-				activeBackgroundColor: "#333333",
-				hoverColor: "#eeeeee",
-				hoverBackgroundColor: "#666666",
-				width: "60px"
-			});
+		onAfterRendering: function() {
+			jQuery.sap.require('openui5.googlemaps.MapUtils');
 
 			var util = openui5.googlemaps.MapUtils;
 			var id = this.createId("mapVbox");
@@ -49,21 +37,21 @@ sap.ui.define([
 			var getLocationCallback = function(oPos) {
 
 				var oMarkers = new openui5.googlemaps.Marker({
-					lat: '{lat}',
-					lng: '{lng}',
-					info: '{name}',
-					icon: icon,
-					animation: 0
+					lat: oPos.lat,
+					lng: oPos.lng,
+					info: "Klinikum GrossHardern"
 				});
+
+				console.log(oMarkers);
 
 				var oMap = new openui5.googlemaps.Map("map1", {
 					lat: oPos.lat,
 					lng: oPos.lng,
 					zoom: 15,
-					markers: {
-						template: oMarkers
-					}
+					markers: [oMarkers]
 				});
+
+				console.log(oMap);
 
 				var oVBox = new sap.m.VBox("oVBox", {
 					items: [oMap]
@@ -73,6 +61,7 @@ sap.ui.define([
 				vBox.addItem(oMap);
 
 				return util.objToLatLng(oPos);
+
 			};
 
 			var updateLocation = function(sLocation) {
@@ -82,8 +71,9 @@ sap.ui.define([
 			util.currentPosition()
 				.then(getLocationCallback)
 				.then(util.geocodePosition)
-				.done(updateLocation);*/
-			}
+				.done(updateLocation);
+
+		}
 
 		/**
 		 * Called when the Controller is destroyed. Use this one to free resources and finalize activities.
