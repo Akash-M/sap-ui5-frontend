f16887bc3ff6f9a33a96b1c3d9fe2b65640d6947
Akash-M <akash.manjunath@gmail.com>
2016-12-15 19:17:52 +0000

    Added tariff pages and confirmation of bike booking.

--- webapp/controller/login.controller.js
+++ webapp/controller/login.controller.js
@@ -4,21 +4,49 @@ sap.ui.define([
 	"use strict";
 
 	return Controller.extend("BikeRentalApp.controller.login", {
-		init : function() {
-		    sap.ui.core.UIComponent.prototype.init.apply(this, arguments);
-		    this.getRouter().initialize();
+		init: function() {
+			sap.ui.core.UIComponent.prototype.init.apply(this, arguments);
+			this.getRouter().initialize();
 		},
-		
-		login: function(){
-			var oRouter = sap.ui.core.UIComponent.getRouterFor(this);
-			oRouter.navTo("dashboard");
+
+		login: function(oEvent) {
+			var oLoginBtn = new sap.m.Button("Login", {
+				text: "Login",
+				press: function() {
+					var oRouter = sap.ui.core.UIComponent.getRouterFor(this);
+					oRouter.navTo("dashboard");
+				}
+			});
+			var oCancelBtn = new sap.m.Button("Cancel", {
+				text: "Cancel",
+				press: function(){
+					sap.ui.getCore().byId("LoginDialog").close();
+				}
+			});
+
+			var oDialog = new sap.m.Dialog("LoginDialog", {
+				title: "Login",
+				modal: true,
+				contentWidth: "1em",
+				buttons: [oLoginBtn, oCancelBtn],
+				content: [
+					new sap.m.Input({
+						placeholder: "Username",
+						id: "username"
+					}),
+					new sap.m.Input({
+						placeholder: "Password",
+						id: "password"
+					})
+				]
+			});
+			sap.ui.getCore().byId("LoginDialog").open();   
 		},
-		
-		nav: function(oEvent){
+
+		nav: function(oEvent) {
 			var route = oEvent.getSource().data("route");
 			var oRouter = sap.ui.core.UIComponent.getRouterFor(this);
-            oRouter.navTo(route);
+			oRouter.navTo(route);
 		}
-		
 	});
 });
